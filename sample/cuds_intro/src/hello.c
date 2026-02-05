395de3f5610a350e1222b6082f0000c2a0fbc086
Raphael CAUSSE <raphael.causse2@gmail.com>
2026-02-05 21:30:37 +0100

    Updated sample project cuds_intro to use CMake
    Links with CUDS library (shared or static), builds and runs

--- sample/cuds_intro/src/hello.c
+++ sample/cuds_intro/src/hello.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+#include <cuds/version.h>
 #include "hello.h"
-#include "cuds/version.h"
 
 void hello(void)
 {
     printf("Welcome to CUDS %s !\n", CUDS_VERSION_STRING);
-    printf("Version  : %d\n", cuds_version());
+    printf("Version  : %08X\n", cuds_version());
     printf("Revision : %s\n", CUDS_REVISION);
 }
