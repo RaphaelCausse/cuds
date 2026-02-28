1ac64efb141478a9665d579b39fd90e8058988be
Raphael CAUSSE <raphael.causse2@gmail.com>
2026-02-28 23:46:24 +0100

    Update headers and sources comment style

--- include/cuds/cuds_api.h
+++ include/cuds/cuds_api.h
@@ -7,32 +7,32 @@
 #ifndef _CUDS_API_H
 #define _CUDS_API_H
 
-//-----------------------------------------------------------------------------
-// Includes
-//-----------------------------------------------------------------------------
+/*****************************************************************************/
+/* INCLUDES */
+/*****************************************************************************/
 
 #include "cuds/cuds_platform_defines.h"
 
-//-----------------------------------------------------------------------------
-// Macros
-//-----------------------------------------------------------------------------
+/*****************************************************************************/
+/* MACROS */
+/*****************************************************************************/
 
 #if defined(CUDS_PLATFORM_WINDOWS)
-  #if defined(CUDS_SHARED) && defined(CUDS_SHARED_EXPORT)
-    /* Building the shared library, exports symbols */
-    #define CUDS_API __declspec(dllexport)
-  #elif defined(CUDS_SHARED) && !defined(CUDS_SHARED_EXPORT)
-    /* Using the shared library, imports symbols */
-    #define CUDS_API __declspec(dllimport)
-  #else
-    #define CUDS_API
-  #endif /* CUDS_SHARED */
-#else
-  #if defined(CUDS_SHARED)
-    #define CUDS_API __attribute__((visibility("default")))
-  #else
-    #define CUDS_API
-  #endif /* CUDS_SHARED */
+#if defined(CUDS_SHARED) && defined(CUDS_SHARED_EXPORT)
+/* Building the shared library, exports symbols */
+#define CUDS_API __declspec(dllexport)
+#elif defined(CUDS_SHARED) && !defined(CUDS_SHARED_EXPORT)
+/* Using the shared library, imports symbols */
+#define CUDS_API __declspec(dllimport)
+#else /* CUDS_SHARED */
+#define CUDS_API
+#endif /* CUDS_SHARED */
+#else  /* CUDS_PLATFORM_WINDOWS */
+#if defined(CUDS_SHARED)
+#define CUDS_API __attribute__((visibility("default")))
+#else /* CUDS_SHARED */
+#define CUDS_API
+#endif /* CUDS_SHARED */
 #endif /* CUDS_PLATFORM_WINDOWS */
 
 #endif /* _CUDS_API_H */
--- tests/test_version.c
+++ tests/test_version.c
@@ -5,12 +5,11 @@
 int main(void)
 {
     const uint32_t version_linked = cuds_version();
-    const char *revision_linked = cuds_revision();
 
     printf("CUDS version (linked)   : %d.%d.%d (0x%08X)\n", CUDS_VERSIONNUM_MAJOR(version_linked), CUDS_VERSIONNUM_MINOR(version_linked), CUDS_VERSIONNUM_PATCH(version_linked), version_linked);
     printf("CUDS version (included) : %d.%d.%d (0x%08X)\n", CUDS_VERSION_MAJOR, CUDS_VERSION_MINOR, CUDS_VERSION_PATCH, CUDS_VERSION);
-    printf("CUDS version (string)   : %s\n", CUDS_VERSION_STRING);
-    printf("CUDS revision (linked)  : %s\n", revision_linked);
+    printf("CUDS version (string)   : '%s'\n", CUDS_VERSION_STRING);
+    printf("CUDS revision (linked)  : %s\n", cuds_revision());
 
     if (version_linked != CUDS_VERSION)
     {
