bca8f51122fd8309d61d67328d9131d70688d4d6
Raphael CAUSSE <raphael.causse2@gmail.com>
2026-03-19 21:45:38 +0100

    Updated 'arena' test

--- tests/test_arena.c
+++ tests/test_arena.c
@@ -3,10 +3,12 @@
 #include <string.h>
 #include <cuds/cuds.h>
 
-#define ARENA_SIZE ((size_t)4)
+#define ARENA_SIZE ((size_t)32)
 
 int main(void)
 {
+    int status = EXIT_SUCCESS;
+
     cuds_arena_t *p_arena = cuds_arena_create(ARENA_SIZE);
     if (p_arena == NULL)
     {
@@ -15,30 +17,34 @@ int main(void)
     }
     printf("Created arena of capacity %zu bytes (@0x%p).\n", ARENA_SIZE, (void *)p_arena);
 
-    uint32_t *x1 = cuds_arena_alloc(p_arena, sizeof(uint32_t));
+    uint32_t *x1 = cuds_arena_alloc(p_arena, sizeof(*x1));
     if (x1 == NULL)
     {
         printf("Error %d: Failed to allocated %zu bytes: %s.\n", errno, sizeof(*x1), strerror(errno));
+        status = EXIT_FAILURE;
     }
     else
     {
-        printf("Allocated %zu bytes (@0x%p).\n", sizeof(*x1), (void *)x1);
-        printf("Remaining %zu bytes in arena.\n", cuds_arena_remaining(p_arena));
+        printf(" Allocated %zu bytes (@0x%p), remaining %zu bytes.\n", sizeof(*x1), (void *)x1, cuds_arena_remaining(p_arena));
+        *x1 = 1024;
+        printf(" *x1 = %u\n", *x1);
     }
 
     uint64_t *x2 = cuds_arena_alloc(p_arena, sizeof(*x2));
     if (x2 == NULL)
     {
         printf("Error %d: Failed to allocated %zu bytes: %s.\n", errno, sizeof(*x2), strerror(errno));
+        status = EXIT_FAILURE;
     }
     else
     {
-        printf("Allocated %zu bytes (@0x%p).\n", sizeof(*x2), (void *)x2);
-        printf("Remaining %zu bytes in arena.\n", cuds_arena_remaining(p_arena));
+        printf(" Allocated %zu bytes (@0x%p), remaining %zu bytes.\n", sizeof(*x2), (void *)x2, cuds_arena_remaining(p_arena));
+        *x2 = 2048;
+        printf(" *x2 = %llu\n", *x2);
     }
 
     p_arena = cuds_arena_destroy(p_arena);
     printf("Destroyed arena (@0x%p).\n", (void *)p_arena);
 
-    return EXIT_SUCCESS;
+    return status;
 }
